0b40c0fc0055e7697c8c3707369e9d7bf3602c3b
harol-lml <hapr0023@gmail.com>
2023-05-23 09:00:54 -0500

    Use environment variable to DB connection

--- app/mongo_db.py
+++ app/mongo_db.py
@@ -1,16 +1,17 @@
+import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from flask_cors import CORS
+from dotenv import load_dotenv
 
 
-class mongo_db: 
+class mongo_db:
 
     def getAll(self):
-        uri = "mongodb+srv://<dbName>:<password>@cluster0.e9tjso8.mongodb.net/?retryWrites=true&w=majority"
-
+        dbname = os.getenv('DB_NAME')
+        dbpass = os.getenv('DB_PASSWORD')
+        uri = f"mongodb+srv://{dbname}:{dbpass}@cluster0.e9tjso8.mongodb.net/?retryWrites=true&w=majority"
         # return uri
-
-        # uri = "mongodb+srv://appMongo:<password>@cluster0.e9tjso8.mongodb.net/?retryWrites=true&w=majority"
 
         # Create a new client and connect to the server
         client = MongoClient(uri, server_api=ServerApi('1'))
